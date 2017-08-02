83be0abb3a84e5c8556dc341321d5562b114ced2
Aaron Kaiser <aaron.kaiser@outlook.de>
2017-08-02 20:30:33 +0200

    refs #1000 löschen von Charakteren hinzugefügt

--- app/src/main/java/com/rixxc/decisions/CharakterInfo.java
+++ app/src/main/java/com/rixxc/decisions/CharakterInfo.java
@@ -3,11 +3,18 @@ package com.rixxc.decisions;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.widget.Toast;
+
+import java.io.File;
 
 public class CharakterInfo extends AppCompatActivity {
 
-    String charakterName;
+    private String charakterName;
+    private File obb;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,6 +23,7 @@ public class CharakterInfo extends AppCompatActivity {
         charakterName = getIntent().getExtras().getString("Charakter");
         setTitle(charakterName);
 
+        obb = new File(getObbDir(), "Charactere");
         Toast.makeText(CharakterInfo.this, charakterName, Toast.LENGTH_SHORT).show();
     }
 
@@ -23,5 +31,34 @@ public class CharakterInfo extends AppCompatActivity {
     public void onBackPressed(){
         Intent intent = new Intent(CharakterInfo.this, Charaktere.class);
         startActivity(intent);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu, menu);
+        return true;
+    }
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.löschen:
+                File[] files = obb.listFiles();
+                for(File f : files){
+                    if(f.getName().equals(charakterName + ".chr")){
+                        try {
+                            f.delete();
+                            break;
+                        }catch (Exception e){
+                            Toast.makeText(CharakterInfo.this, "Es ist ein Fehler beim löschen des Charakters aufgetreten", Toast.LENGTH_LONG).show();
+                        }
+                    }
+                }
+
+                Intent intent = new Intent(CharakterInfo.this, Charaktere.class);
+                startActivity(intent);
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
     }
 }
