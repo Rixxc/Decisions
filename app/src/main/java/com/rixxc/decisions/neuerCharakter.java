bd2112857384f386d1ec8775e560c8326013f910
Aaron Kaiser <aaron.kaiser@outlook.de>
2017-08-03 20:48:11 +0200

    refs #1000 Eigenschaftspunkte Info hinzugefügt

--- app/src/main/java/com/rixxc/decisions/neuerCharakter.java
+++ app/src/main/java/com/rixxc/decisions/neuerCharakter.java
@@ -3,9 +3,12 @@ package com.rixxc.decisions;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.KeyEvent;
+import android.view.MotionEvent;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import java.io.BufferedWriter;
@@ -19,7 +22,30 @@ public class neuerCharakter extends AppCompatActivity {
 
     private Button create;
     private EditText name,stärke,ausdauer,intelligenz,geschicklichkeit,mut;
+    private TextView punkte;
     private File obb;
+    private TextView.OnEditorActionListener onEdit = new TextView.OnEditorActionListener() {
+        @Override
+        public boolean onEditorAction(TextView v, int actionId, KeyEvent event) {
+            int istärke,iausdauer,imut,iintelligenz,igeschicklichkeit;
+            istärke = Integer.parseInt(stärke.getText().toString());
+            iausdauer = Integer.parseInt(ausdauer.getText().toString());
+            imut = Integer.parseInt(mut.getText().toString());
+            iintelligenz = Integer.parseInt(intelligenz.getText().toString());
+            igeschicklichkeit = Integer.parseInt(geschicklichkeit.getText().toString());
+
+            int sum = 15 - ((istärke+iausdauer+imut+iintelligenz+igeschicklichkeit) - 50);
+
+            if(istärke < 10 || imut < 10 || iausdauer < 10 || iintelligenz < 10 || igeschicklichkeit < 10){
+                punkte.setText("Keiner der Werte darf unter 10 liegen");
+            }else if(sum < 0){
+                punkte.setText("Es wurden zu viele Punkte ausgegeben");
+            }else{
+                punkte.setText("Verfügbare Punkte : " + sum);
+            }
+            return false;
+        }
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,6 +54,7 @@ public class neuerCharakter extends AppCompatActivity {
         setTitle("Charaktere");
 
         obb = new File(getObbDir(), "Charactere");
+        punkte = (TextView) findViewById(R.id.LPunkte);
         name = (EditText) findViewById(R.id.Name);
         stärke = (EditText) findViewById(R.id.stärke);
         ausdauer = (EditText) findViewById(R.id.ausdauer);
@@ -35,6 +62,18 @@ public class neuerCharakter extends AppCompatActivity {
         intelligenz = (EditText) findViewById(R.id.intelligenz);
         geschicklichkeit = (EditText) findViewById(R.id.geschicklichkeit);
         create = (Button) findViewById(R.id.createCharakter);
+
+        stärke.setText("10");
+        ausdauer.setText("10");
+        mut.setText("10");
+        intelligenz.setText("10");
+        geschicklichkeit.setText("10");
+
+        stärke.setOnEditorActionListener(onEdit);
+        ausdauer.setOnEditorActionListener(onEdit);
+        mut.setOnEditorActionListener(onEdit);
+        intelligenz.setOnEditorActionListener(onEdit);
+        geschicklichkeit.setOnEditorActionListener(onEdit);
 
         create.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -57,11 +96,14 @@ public class neuerCharakter extends AppCompatActivity {
                                 iintelligenz = Integer.parseInt(intelligenz.getText().toString());
                                 igeschicklichkeit = Integer.parseInt(geschicklichkeit.getText().toString());
 
-                                if((istärke+iausdauer+imut+iintelligenz+igeschicklichkeit) > 15){
+                                if((istärke+iausdauer+imut+iintelligenz+igeschicklichkeit) > 65){
                                     throw new Exception("Es wurden zu viele Punkte verteilt");
                                 }
                                 if(name.getText().toString() == "" || name.getText().toString() == null || name.getText().toString().contains(" ")){
                                     throw new Exception("Ungültiger Name");
+                                }
+                                if(istärke < 10 || imut < 10 || iausdauer < 10 || iintelligenz < 10 || igeschicklichkeit < 10){
+                                    throw new Exception("Kein Wert darf unter 10 liegen");
                                 }
 
                                 FileWriter fw = new FileWriter(charakter);
@@ -85,7 +127,7 @@ public class neuerCharakter extends AppCompatActivity {
 
                                 charakter.createNewFile();
                             }catch (Exception e) {
-                                if(e.getMessage().equals("Es wurden zu viele Punkte verteilt") || e.getMessage().equals("Ungültiger Name")){
+                                if(e.getMessage().equals("Es wurden zu viele Punkte verteilt") || e.getMessage().equals("Ungültiger Name") || e.getMessage().equals("Kein Wert darf unter 10 liegen")){
                                     Toast.makeText(neuerCharakter.this, e.getMessage(), Toast.LENGTH_LONG).show();
                                 }else {
                                     Toast.makeText(neuerCharakter.this, "Es ist ein Fehler beim erstellen des Charakters aufgetreten", Toast.LENGTH_LONG).show();
